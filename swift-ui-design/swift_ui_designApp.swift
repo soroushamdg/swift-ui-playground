cfeec390a8e06c31921cc3a2651dfd9ab2e8f81c
soroushamdg <soroushmpour@gmail.com>
2022-10-29 18:13:45 -0400

    Initial Commit

---
+++ swift-ui-design/swift_ui_designApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_ui_designApp.swift
+//  swift-ui-design
+//
+//  Created by Soro on 2022-10-29.
+//
+
+import SwiftUI
+
+@main
+struct swift_ui_designApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
